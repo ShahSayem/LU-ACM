56c58b3158610bb974bc5fdb0cf23d076142aa98
Shah Sayem Ahmad <shahsayemahmad@gmail.com>
2022-12-25 01:30:59 +0600

    X-mass day

--- ZeroToOne-MyMentorship/One.c
+++ ZeroToOne-MyMentorship/One.c
@@ -1,14 +1,13 @@
-#include <stdio.h>
- 
-int main() 
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
 {
- 
-    int a, b, x;
-    scanf("%d", &a);
-    scanf("%d", &b);
-    
-    x = a+b;
-    printf("X = %d\n", x);
- 
+    int a,b,c,y,x;
+    scanf("%d %d %d", &a, &b, &c);
+    y=(a+b+abs(a-b))/2;
+    x=(y+c+abs(y-c))/2;
+    printf("%d eh o maior\n", x);
+
     return 0;
 }
