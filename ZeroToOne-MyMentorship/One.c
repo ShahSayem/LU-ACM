4e6f7d64103cb0b6a87fd76056ebd1fc0faf8926
Shah Sayem Ahmad <shahsayemahmad@gmail.com>
2023-01-01 16:54:13 +0600

    New year

--- ZeroToOne-MyMentorship/One.c
+++ ZeroToOne-MyMentorship/One.c
@@ -1,13 +1,25 @@
 #include<stdio.h>
-#include<stdlib.h>
-
 int main()
 {
-    int a,b,c,y,x;
-    scanf("%d %d %d", &a, &b, &c);
-    y=(a+b+abs(a-b))/2;
-    x=(y+c+abs(y-c))/2;
-    printf("%d eh o maior\n", x);
+    int n;
+    scanf("%d",&n);
+
+    printf("%d\n",n);
+
+    printf("%d nota(s) de R$ 100,00\n",n/100);
+    n %= 100;
+    printf("%d nota(s) de R$ 50,00\n",n/50);
+    n %= 50;
+    printf("%d nota(s) de R$ 20,00\n",n/20);
+    n %= 20;
+    printf("%d nota(s) de R$ 10,00\n",n/10);
+    n %= 10;
+    printf("%d nota(s) de R$ 5,00\n",n/5);
+    n %= 5;
+    printf("%d nota(s) de R$ 2,00\n",n/2);
+    n %= 2;
+    printf("%d nota(s) de R$ 1,00\n",n/1);
+    n %= 1;
 
     return 0;
 }
